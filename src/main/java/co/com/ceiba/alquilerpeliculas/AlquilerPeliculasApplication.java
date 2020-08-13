c31da6bc841bf61c0ede6917a110733b29d365b8
Claudia Ximena Bustamante Gómez - Ceiba Software <claudia.bustamante@ceiba.com.co>
2020-08-13 10:34:15 -0500

    Creación Carpetas Proyecto

---
+++ src/main/java/co/com/ceiba/alquilerpeliculas/AlquilerPeliculasApplication.java
@@ -0,0 +1,13 @@
+package co.com.ceiba.alquilerpeliculas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlquilerPeliculasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlquilerPeliculasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/ceiba/alquilerpeliculas/AlquilerPeliculasApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.ceiba.alquilerpeliculas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlquilerPeliculasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
