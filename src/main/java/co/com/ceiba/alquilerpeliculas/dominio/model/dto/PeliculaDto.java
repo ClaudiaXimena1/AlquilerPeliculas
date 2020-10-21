74a20c72b21cbf3748590043b941d5f438dc928e
Claudia Bustamante <claudiaximena1@hotmail.com>
2020-10-21 13:02:31 -0500

    Ajustes revisión Sonar

--- src/main/java/co/com/ceiba/alquilerpeliculas/dominio/model/dto/PeliculaDto.java
+++ src/main/java/co/com/ceiba/alquilerpeliculas/dominio/model/dto/PeliculaDto.java
@@ -1,23 +1,14 @@
 package co.com.ceiba.alquilerpeliculas.dominio.model.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@AllArgsConstructor
 @Getter
 public class PeliculaDto {
 
 	private String nombre;
 	private String genero;
 	private String duracion;
-
-	public PeliculaDto() {
-		super();
-	}
-
-	public PeliculaDto(String nombre, String genero, String duracion) {
-		super();
-		this.nombre = nombre;
-		this.genero = genero;
-		this.duracion = duracion;
-	}
 
 }
