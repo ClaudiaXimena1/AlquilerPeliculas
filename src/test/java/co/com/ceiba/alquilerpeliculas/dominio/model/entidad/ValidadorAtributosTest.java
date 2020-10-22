93ca4c4f8c2f8abdd5f23743eea32934a01055ee
Claudia Bustamante <claudiaximena1@hotmail.com>
2020-10-22 11:05:24 -0500

    Ajustes Revisión Sonar

--- src/test/java/co/com/ceiba/alquilerpeliculas/dominio/model/entidad/ValidadorAtributosTest.java
+++ src/test/java/co/com/ceiba/alquilerpeliculas/dominio/model/entidad/ValidadorAtributosTest.java
@@ -1,18 +1,32 @@
 package co.com.ceiba.alquilerpeliculas.dominio.model.entidad;
 
+import static org.junit.Assert.fail;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class ValidadorAtributosTest {
 
 	@Test
-	public void validarAtributosTest() {
+	public void validarAtributosNullTest() {
 		final String mensageObligatorio = "El nombre es un dato obligatorio.";
 
 		try {
 			ValidadorAtributos.validarAtributos(null, mensageObligatorio);
 		} catch (Exception e) {
 			Assert.assertTrue(e.getMessage().contains(mensageObligatorio));
+		}
+
+	}
+	
+	@Test
+	public void validarAtributosNotNullTest() {
+		final String mensageObligatorio = "El nombre es un dato obligatorio.";
+
+		try {
+			ValidadorAtributos.validarAtributos("Titanic", mensageObligatorio);
+		} catch (Exception e) {
+			fail("Should not have thrown any exception");
 		}
 
 	}
