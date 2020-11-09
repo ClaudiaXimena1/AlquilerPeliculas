4e4016d67b1e325e230dc4f4010532aecc66586e
Claudia Bustamante <claudiaximena1@hotmail.com>
2020-11-09 09:49:09 -0500

    Modificacion import.sql

--- src/test/resources/import.sql
+++ src/test/resources/import.sql
@@ -1,4 +1,4 @@
 INSERT INTO pelicula_entity(nombre, genero, duracion) VALUES ('El Cuaderno de Sara','Drama','2 horas 10 minutos');
 INSERT INTO pelicula_entity(nombre, genero, duracion) VALUES ('Sombra en la Ciudad','Suspenso','1 hora 45 minutos');
 INSERT INTO pelicula_entity(nombre, genero, duracion) VALUES ('Batman','Accion','2 horas 15 minutos');
-INSERT INTO cliente_entity(identificacion, nombres, apellidos, direccion, telefono_fijo, telefono_movil) VALUES ('31274274', 'Argenis', 'Gómez Grajales', 'Calle 8 con 7', '2222222', '3175181818');
+INSERT INTO cliente_entity(identificacion, nombres, apellidos, direccion, telefono_fijo, telefono_movil) VALUES ('31274274', 'Argenis', 'Gomez Grajales', 'Calle 8 con 7', '2222222', '3175181818');
