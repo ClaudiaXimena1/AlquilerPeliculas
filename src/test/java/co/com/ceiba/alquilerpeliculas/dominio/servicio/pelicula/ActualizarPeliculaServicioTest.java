1d3c36d581ddeb7f2afc8c19e7be238aa5e36718
Claudia Bustamante <claudiaximena1@hotmail.com>
2020-11-09 13:35:19 -0500

    Modificacion prueba ActualizarPeliculaServicioTest

--- src/test/java/co/com/ceiba/alquilerpeliculas/dominio/servicio/pelicula/ActualizarPeliculaServicioTest.java
+++ src/test/java/co/com/ceiba/alquilerpeliculas/dominio/servicio/pelicula/ActualizarPeliculaServicioTest.java
@@ -1,7 +1,10 @@
 package co.com.ceiba.alquilerpeliculas.dominio.servicio.pelicula;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.function.Supplier;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -12,6 +15,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import co.com.ceiba.alquilerpeliculas.dominio.excepcion.ValidaExistenciaExcepcion;
 import co.com.ceiba.alquilerpeliculas.dominio.model.dto.PeliculaDto;
 import co.com.ceiba.alquilerpeliculas.dominio.model.entidad.Pelicula;
 import co.com.ceiba.alquilerpeliculas.dominio.testdatabuilder.PeliculaTestDataBuilder;
@@ -65,6 +69,30 @@ public class ActualizarPeliculaServicioTest {
 		}	
 
 		
+	}
+	
+	@Test
+	public void validaNoExistePeli2Test() {
+		
+		final String mensage = "La pelicula no existe en el sistema";
+		
+		PeliculaTestDataBuilder peliculaTestDataBuilder = new PeliculaTestDataBuilder();
+		peliculaTestDataBuilder.setId((long) 8);
+		peliculaTestDataBuilder.setNombre("La Prueba");
+		
+		assertThrows(() -> actualizarPeliculaServicio.ejecutar(peliculaTestDataBuilder.build()), ValidaExistenciaExcepcion.class, mensage);
+				
+	}
+	
+	private static <T> void assertThrows(Supplier<T> supplier, Class<? extends Exception> exception, String message) {
+		try {
+			supplier.get();
+			fail();
+		} catch (Exception e) {
+			Assert.assertTrue("Se esperaba la excepcion" + exception.getCanonicalName() + "pero fue lanzada"
+					+ e.getClass().getCanonicalName(), exception.isInstance(e));
+			Assert.assertTrue(e.getMessage().contains(message));
+		}
 	}
 
 }
