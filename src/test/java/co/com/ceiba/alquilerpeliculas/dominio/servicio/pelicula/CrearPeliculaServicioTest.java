3ed95c24df11c55a0519c32cfc5b22e3e9bb9b0c
Claudia Bustamante <claudiaximena1@hotmail.com>
2020-11-13 07:26:56 -0500

    Modificacion Test CrearPeliculaServicioTest

--- src/test/java/co/com/ceiba/alquilerpeliculas/dominio/servicio/pelicula/CrearPeliculaServicioTest.java
+++ src/test/java/co/com/ceiba/alquilerpeliculas/dominio/servicio/pelicula/CrearPeliculaServicioTest.java
@@ -1,6 +1,9 @@
 package co.com.ceiba.alquilerpeliculas.dominio.servicio.pelicula;
 
+import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.function.Supplier;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -10,8 +13,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import co.com.ceiba.alquilerpeliculas.dominio.excepcion.ValidaExistenciaExcepcion;
 import co.com.ceiba.alquilerpeliculas.dominio.model.dto.PeliculaDto;
-import co.com.ceiba.alquilerpeliculas.dominio.model.entidad.Pelicula;
 import co.com.ceiba.alquilerpeliculas.dominio.testdatabuilder.PeliculaTestDataBuilder;
 
 @RunWith(SpringRunner.class)
@@ -38,15 +41,23 @@ public class CrearPeliculaServicioTest {
 	public void validaExistePeliTest() {
 
 		final String mensage = "La pelicula ya existe en el sistema";
-		Pelicula pelicula = new Pelicula(null, "Batman", "Accion", "2 horas 15 minutos");
+		PeliculaTestDataBuilder peliculaTestDataBuilder = new PeliculaTestDataBuilder();
+		peliculaTestDataBuilder.setNombre("El Cuaderno de Sara");
 
+		assertThrows(() -> crearPeliculaServicio.ejecutar(peliculaTestDataBuilder.build()),
+				ValidaExistenciaExcepcion.class, mensage);
+
+	}
+
+	private static <T> void assertThrows(Supplier<T> supplier, Class<? extends Exception> exception, String message) {
 		try {
-			crearPeliculaServicio.ejecutar(pelicula);
-
+			supplier.get();
+			fail();
 		} catch (Exception e) {
-			Assert.assertTrue(e.getMessage().contains(mensage));
+			Assert.assertTrue("Se esperaba la excepcion" + exception.getCanonicalName() + "pero fue lanzada"
+					+ e.getClass().getCanonicalName(), exception.isInstance(e));
+			Assert.assertTrue(e.getMessage().contains(message));
 		}
-
 	}
 
 }
